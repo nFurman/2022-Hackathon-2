f2b5f5d2506546edba3751e1d3d954eb9afdbfc5
nFurman <115150157+nFurman@users.noreply.github.com>
2022-11-16 22:48:08 +0200

    added pieces

--- resources.js
+++ resources.js
@@ -1,6 +1,18 @@
-//turn assets into js consts
+//asset dictionaries
+const whitePieces = {
+  r: "assets/white_rook.png",
+  p: "assets/white_pawn.png",
+  n: "assets/white_knight.png",
+  b: "assets/white_bishop.png",
+  q: "assets/white_queen.png",
+  k: "assets/white_king.png",
+};
 
-const assetDictionary = {
-  //"p": "assets/pawn.png"
-  //...etc
+const blackPieces = {
+  r: "assets/black_rook.png",
+  p: "assets/black_pawn.png",
+  n: "assets/black_knight.png",
+  b: "assets/black_bishop.png",
+  q: "assets/black_queen.png",
+  k: "assets/black_king.png",
 };
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const BOARD_ELEMENT = document.getElementById("board_container");
 
 const TILE_SIZE = 50;
-const TILE_GAP = 10;
+const TILE_GAP = 0;
 
 const BOARD_SIZE = TILE_SIZE * 8 + TILE_GAP * 7;
 BOARD_ELEMENT.style.height = BOARD_SIZE + "px";
@@ -26,33 +26,64 @@ function makeEmptyBoard() {
   for (let r = 0; r < 8; r++) {
     let col = [];
     for (let c = 0; c < 8; c++) {
-      col.push(" ");
+      col.push(["empty"]);
     }
     resultingBoard.push(col);
   }
   return resultingBoard;
 }
 
-function drawStartPieces(board) {}
+function drawPiece(color, piece, row, col) {
+  console.log(
+    "trying to draw a " + color + " " + piece + " at " + row + " " + col
+  );
+  let pieceImage = document.createElement("img");
+  pieceImage.src = color === "white" ? whitePieces[piece] : blackPieces[piece];
+  pieceImage.className = "piece";
+  pieceImage.width = TILE_SIZE;
+  pieceImage.height = TILE_SIZE;
+  pieceImage.style.left = TILE_SIZE * col + TILE_GAP * col + "px";
+  pieceImage.style.top = TILE_SIZE * row + TILE_GAP * row + "px";
+
+  BOARD_ELEMENT.appendChild(pieceImage);
+}
+
+function drawStartPieces(board) {
+  for (r in board) {
+    for (c in board[r]) {
+      let piece = board[r][c]; //either ["empty"] or ["color", "piece"]
+      if (board[r][c] != "empty") {
+        drawPiece(board[r][c][0], board[r][c][1], r, c);
+      }
+    }
+  }
+}
 
 function setUpBoard() {
   board = makeEmptyBoard();
   for (let c = 0; c < 8; c++) {
-    board[1][c] = "p";
-    board[6][c] = "p";
+    board[1][c] = ["black", "p"];
+    board[6][c] = ["white", "p"];
   }
-  for (let x of [0, 7]) {
-    board[x][0] = "r";
-    board[x][7] = "r";
-    board[x][1] = "n";
-    board[x][6] = "n";
-    board[x][2] = "b";
-    board[x][5] = "b";
-  }
-  board[0][4] = "q";
-  board[0][3] = "k";
-  board[7][3] = "q";
-  board[7][4] = "k";
+
+  board[0][0] = ["black", "r"];
+  board[0][7] = ["black", "r"];
+  board[0][1] = ["black", "n"];
+  board[0][6] = ["black", "n"];
+  board[0][2] = ["black", "b"];
+  board[0][5] = ["black", "b"];
+
+  board[7][0] = ["white", "r"];
+  board[7][7] = ["white", "r"];
+  board[7][1] = ["white", "n"];
+  board[7][6] = ["white", "n"];
+  board[7][2] = ["white", "b"];
+  board[7][5] = ["white", "b"];
+
+  board[0][4] = ["black", "q"];
+  board[0][3] = ["black", "k"];
+  board[7][3] = ["white", "q"];
+  board[7][4] = ["white", "k"];
 
   drawStartPieces(board);
 }
