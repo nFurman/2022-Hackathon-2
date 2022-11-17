26b50bcfff2479f81c73bcfc911805900a91215b
Thecoder03 <amdurzachary@gmail.com>
2022-11-17 14:53:54 +0200

    New file system, NodeJS with websocket added

---
+++ server.js
@@ -0,0 +1,30 @@
+const { Socket } = require("engine.io");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/homepage", (req, res) => {
+  res.sendFile(__dirname + "/public/game.html");
+  console.log("on the homepage");
+});
+
+io.on("connection", (Socket) => {
+  console.log("User connected:" + Socket.id);
+  //console.log(Socket);
+
+  Socket.on("message", (data) => {
+    Socket.broadcast.emit("message", data);
+    console.log(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on 3000");
+});
